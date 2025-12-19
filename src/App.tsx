a26c0d0dbed6c4af6000a4e29ae44eca0daa6318
lostmart <martinpedraza1979@gmail.com>
2025-12-19 18:50:50 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as images from "./assets/imgs/images"
+import "./App.css"
+
+const App = () => {
+	return (
+		<section className="min-h-screen flex items-center justify-center p-2">
+			<div className="w-full max-w-[500px] h-[498px] relative frame">
+				<div className="sky w-full h-[382px]" ></div>
+			</div>
+		</section>
+	)
+}
+
+export default App
---
+++ src/assets/imgs/images.ts
@@ -0,0 +1,7 @@
+import barco from "./barco.svg"
+import earth from "./earth.svg"
+import mountains from "./mountains.svg"
+import trees from "./trees.svg"
+import waterReflex from "./water-reflex.svg"
+
+export { barco, earth, mountains, trees, waterReflex }
