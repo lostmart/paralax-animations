860d2df5e1ab9bc035a849cacfd1a39804ff28ad
lostmart <martinpedraza1979@gmail.com>
2025-12-19 21:00:47 +0100

    new images

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import "./App.css"
 const App = () => {
 	return (
 		<section className="min-h-screen flex items-center justify-center p-2">
-			<div className="w-full max-w-[500px] h-[498px] relative frame">
-				<div className="sky w-full h-[382px]" ></div>
+			<div className="w-full max-w-[500px] h-[498px] frame">
+				<div className="sky relative w-full h-[382px]">
+					<div className="moon absolute"></div>
+					<div className="trees absolute"></div>
+					<div className="terrain absolute"></div>
+					<div className="boat absolute"></div>
+					<div className="mountains absolute"></div>
+					<div className="lake absolute w-full h-[192px] -bottom-30"></div>
+					<div className="reflections absolute w-full"></div>
+
+				</div>
 			</div>
 		</section>
 	)
