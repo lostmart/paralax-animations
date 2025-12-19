de4bff5213f4fe63cee5b75d55cfbd37ee7cd016
lostmart <martinpedraza1979@gmail.com>
2025-12-19 21:28:27 +0100

    framer motion mouse effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import * as images from "./assets/imgs/images"
 import "./App.css"
+import { motion, useMotionValue, useTransform } from "framer-motion"
+import { useEffect } from "react"
 
 const App = () => {
+	const mouseY = useMotionValue(0)
+
+	useEffect(() => {
+		const handleMouseMove = (e: MouseEvent) => {
+			mouseY.set(e.clientY)
+		}
+
+		window.addEventListener("mousemove", handleMouseMove)
+		return () => window.removeEventListener("mousemove", handleMouseMove)
+	}, [mouseY])
+
+	// Transform mouse Y (0 to window height) into moon movement
+	const moonY = useTransform(mouseY, [0, window.innerHeight], [20, -20])
+
 	return (
 		<section className="min-h-screen flex items-center justify-center p-2">
 			<div className="w-full max-w-[500px] h-[498px] frame">
 				<div className="sky relative w-full h-[382px]">
-					<div className="moon absolute"></div>
+					<motion.div style={{ y: moonY }} className="moon"></motion.div>
 					<div className="trees absolute"></div>
 					<div className="terrain absolute"></div>
 					<div className="boat absolute"></div>
 					<div className="mountains absolute"></div>
 					<div className="lake absolute w-full h-[192px] -bottom-30"></div>
 					<div className="reflections absolute w-full"></div>
-
 				</div>
 			</div>
 		</section>
